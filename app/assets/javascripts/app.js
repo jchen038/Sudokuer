49caa73b1141b1b24acf75af179b9bb9e8b4a10a
Jimmy <jchen@drmcnatty.com>
2015-01-31 18:06:27 -0800

    input setup

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,7 +1,19 @@
 $(document).ready(function(){
-    $(".button-select").click(function() {
-      var currentSquare = this;
-      $(".button-select").removeClass("Active");
-      $(currentSquare).addClass("Active");
-    });
+  // Puzzle functionality
+  $(".button-select").click(function() {
+    var currentSquare = this;
+    $(".button-select").removeClass("Active");
+    $(currentSquare).addClass("Active");
+  });
+
+  $(".buttons").click(function() {
+    var currentButton = this;
+    var currentButtonValue = $(this).text().trim();
+    var activeClasses = $.inArray( "base_cell", $(".Active").attr('class').split(/\s+/)) < 0;
+    console.log(activeClasses);
+    if (activeClasses) {
+      $(".Active").text("");
+      $(".Active").text(currentButtonValue);
+    }
+  });
 });
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128034230) do
+ActiveRecord::Schema.define(version: 20150201013136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20150128034230) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "puzzle_id"
+    t.boolean  "base_cell"
   end
 
   create_table "puzzles", force: :cascade do |t|
