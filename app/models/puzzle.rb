1973b668228601f286274503cf8a188c15088577
Jimmy <jchen@drmcnatty.com>
2015-02-02 17:55:08 -0800

    algorithm (wip)

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -39,15 +39,15 @@ class Puzzle < ActiveRecord::Base
   end
 
   def solve
-    puzzle = [  {row: [1,2,3], column: [1,2,3]},  # 1
-                {row: [1,2,3], column: [4,5,6]},  # 2
-                {row: [1,2,3], column: [7,8,9]},  # 3
-                {row: [4,5,6], column: [1,2,3]},  # 4
-                {row: [4,5,6], column: [4,5,6]},  # 5
-                {row: [4,5,6], column: [7,8,9]},  # 6
-                {row: [7,8,9], column: [1,2,3]},  # 7
-                {row: [7,8,9], column: [4,5,6]},  # 8
-                {row: [7,8,9], column: [7,8,9]}]  # 9
+    puzzle = [ {block: 1, row: [1,2,3], column: [1,2,3]},  # 1
+               {block: 2, row: [1,2,3], column: [4,5,6]},  # 2
+               {block: 3, row: [1,2,3], column: [7,8,9]},  # 3
+               {block: 4, row: [4,5,6], column: [1,2,3]},  # 4
+               {block: 5, row: [4,5,6], column: [4,5,6]},  # 5
+               {block: 6, row: [4,5,6], column: [7,8,9]},  # 6
+               {block: 7, row: [7,8,9], column: [1,2,3]},  # 7
+               {block: 8, row: [7,8,9], column: [4,5,6]},  # 8
+               {block: 9, row: [7,8,9], column: [7,8,9]}]  # 9
     puzzle.each do |p|
 
     end
