81c525e0b170295355c1d8dfed3c9c17d9234ceb
jimmy <mfwinds@gmail.com>
2015-04-03 17:45:35 -0700

    Cell: get_block with tests

--- app/models/cell.rb
+++ app/models/cell.rb
@@ -1,3 +1,21 @@
 class Cell < ActiveRecord::Base
   belongs_to :puzzle
+
+  def self.get_block(row, column)
+    block = 0
+    if [4,5,6].include?(row)
+      block += 3
+    elsif [7,8,9].include?(row)
+      block += 6
+    end
+
+    if [1,2,3].include?(column)
+      block += 1
+    elsif [4,5,6].include?(column)
+      block += 2
+    elsif [7,8,9].include?(column)
+      block += 3
+    end
+    block
+  end
 end
--- test/models/cell_test.rb
+++ test/models/cell_test.rb
@@ -4,4 +4,11 @@ class CellTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :cells
+  test "get cell block" do
+    cell = cells(:one)
+    cell_two = cells(:two)
+    assert Cell.get_block(cell.row, cell.column) == cell.block, "Cell: get_block did not return correct block value"
+    assert Cell.get_block(cell_two.row, cell_two.column) == cell_two.block, "Cell Two did not return correct block value"
+  end
 end
