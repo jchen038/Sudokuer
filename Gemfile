37fef637028b417cb2d16a05ec31119cc3562840
Jimmy Chen <jimmyc@fair.com>
2018-09-05 18:50:02 -0700

    use pg 18 with 5.2 migration

--- Gemfile
+++ Gemfile
@@ -4,13 +4,13 @@ source 'https://rubygems.org'
 gem 'rails', '5.2.0'
 gem 'sass-rails'
 gem 'bootstrap-sass'
-gem 'pg'
+gem 'pg', '~> 0.18'
 gem 'haml'
 gem 'jquery-rails'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0',          group: :doc
 
-gem 'rails_12factor', group: :production
+# gem 'rails_12factor', group: :production
 
 group :development, :test do
   gem 'rspec-rails'
--- spec/coordinators/puzzles/build_coordinator_spec.rb
+++ spec/coordinators/puzzles/build_coordinator_spec.rb
@@ -4,13 +4,11 @@ RSpec.describe Puzzles::BuildCoordinator do
   describe '#call' do
     let(:puzzle) { FactoryBot.create(:puzzle, :complete) }
     let(:coordinator) do
-      described_class.new(
-        puzzle: puzzle
-      )
+      described_class.new(puzzle: puzzle)
     end
 
-    context '' do
-      it '' do
+    context 'with full puzzle' do
+      it 'returns puzzle with updated cells and all possibility' do
         coordinator.call
         puzzle.reload
         expect().to
